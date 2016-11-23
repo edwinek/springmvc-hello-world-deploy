36680a65e2a744f9a990ae034c023d1c900afcb3
eddie <eddie@example.com>
2016-11-23 07:15:25 +0000

    Updating to use automated DockerHub builds.

--- deploy.sh
+++ deploy.sh
@@ -15,14 +15,14 @@ function cleanup_containers_and_files() {
 
 cleanup_containers_and_files
 
-docker run --name hw_getter_container -d edwinek/alpine-git:latest
+docker run --name hw_getter_container -tid edwinek/alpine-git:latest sh
 docker exec -ti hw_getter_container git clone $PROJECT_URL /opt/src/$PROJECT_NAME
 docker exec -ti hw_getter_container sh -c "cd /opt/src/$PROJECT_NAME && git archive -o /tmp/$SRC_ARCHIVE master"
 docker cp hw_getter_container:/tmp/$SRC_ARCHIVE builder/$SRC_ARCHIVE
 docker stop hw_getter_container
 
 docker build -t hw_builder_image builder/.
-docker run --name hw_builder_container -d hw_builder_image
+docker run --name hw_builder_container -tid hw_builder_image sh
 docker cp hw_builder_container:/opt/src/target/$WAR_FILE deployer/$WAR_FILE
 docker stop hw_builder_container
 
